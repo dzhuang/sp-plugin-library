43e058e78ca436b422e53b4b1d9743c9296b6d1a
slava.probeez <slava.probeez@gmail.com>
2011-02-18 14:41:42 +0000

    Initial import

---
+++ src/com/probeez/profiles/plugin/PluginBroadcastReceiver.java
@@ -0,0 +1,82 @@
+package com.probeez.profiles.plugin;
+
+import static android.app.Activity.RESULT_OK;
+import static com.probeez.profiles.plugin.PluginIntent.ACTION_CHECK_CONDITION;
+import static com.probeez.profiles.plugin.PluginIntent.ACTION_PERFORM_ACTION;
+import static com.probeez.profiles.plugin.PluginIntent.ACTION_QUERY_PLUGIN_STATUS;
+import static com.probeez.profiles.plugin.PluginIntent.ACTION_START_PLUGIN;
+import static com.probeez.profiles.plugin.PluginIntent.ACTION_STOP_PLUGIN;
+import static com.probeez.profiles.plugin.PluginIntent.EXTRA_PLUGIN_STATUS;
+import static com.probeez.profiles.plugin.PluginIntent.EXTRA_STATE;
+import static com.probeez.profiles.plugin.PluginIntent.EXTRA_STATES;
+
+import java.util.Set;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public abstract class PluginBroadcastReceiver extends BroadcastReceiver {
+	public static final int STATUS_UNKNOWN = -1;	
+	public static final int STATUS_STARTED = 0;
+	public static final int STATUS_STOPPED = 1;
+	static final String TAG = "SPPlugin";
+	/** to enable debug run 'adb shell setprop log.tag.SPPlugin DEBUG' and reinstall/restart a plugin */
+	static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG); 
+
+	
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		String action = intent.getAction();
+		if (DEBUG) Log.d(TAG, "Got SP broadcast:"+action);
+		if (ACTION_START_PLUGIN.equals(action)) {
+			onStartPlugin(context);
+		} 
+		else if (ACTION_STOP_PLUGIN.equals(action)) {
+			onStopPlugin(context);
+		} 
+		else if (ACTION_QUERY_PLUGIN_STATUS.equals(action)) {
+			int status = onQueryPluginStatus(context);
+			Bundle results = new Bundle();
+			results.putInt(EXTRA_PLUGIN_STATUS, status);
+			setResult(RESULT_OK, null, results);
+		} 
+		else if (ACTION_CHECK_CONDITION.equals(action)) {
+			Bundle sourceState = intent.getBundleExtra(EXTRA_STATE);
+			Bundle targetStates = intent.getBundleExtra(EXTRA_STATES);
+			Set<String> keys = targetStates.keySet();
+			Bundle results = new Bundle(keys.size());
+			for (String key : keys) {
+				Bundle targetState = targetStates.getBundle(key);
+				boolean result = onCheckCondition(context, sourceState, targetState);
+				if (DEBUG) Log.d(TAG, "Check condition:"+key+", result:"+result);
+				results.putBoolean(key, result);
+			}
+			setResult(RESULT_OK, null, results);
+		} 
+		else if (ACTION_PERFORM_ACTION.equals(action)) {
+			Bundle state = intent.getBundleExtra(EXTRA_STATE);
+			onPerformAction(context, state);
+		}
+	}
+
+  protected boolean onCheckCondition(Context context, Bundle sourceState, Bundle targetState) {
+  	return false;
+  }
+  
+  protected void onPerformAction(Context context, Bundle state) {
+  }
+  
+  protected void onStartPlugin(Context context) {
+  }
+
+  protected void onStopPlugin(Context context) {
+  }
+  
+  protected int onQueryPluginStatus(Context context) {
+  	return STATUS_UNKNOWN;
+  }
+	
+}
---
+++ src/com/probeez/profiles/plugin/PluginIntent.java
@@ -0,0 +1,52 @@
+package com.probeez.profiles.plugin;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.os.Bundle;
+
+public class PluginIntent extends Intent {
+	public static final String ACTION_EDIT_CONDITION = "com.probeez.profiles.plugin.EDIT_CONDITION";
+	public static final String ACTION_EDIT_ACTION = "com.probeez.profiles.plugin.EDIT_ACTION";
+	public static final String ACTION_TRIGGER_CONDITION = "com.probeez.profiles.plugin.TRIGGER_CONDITION";
+	public static final String ACTION_CHECK_CONDITION = "com.probeez.profiles.plugin.CHECK_CONDITION";
+	public static final String ACTION_PERFORM_ACTION = "com.probeez.profiles.plugin.PERFORM_ACTION";
+	public static final String ACTION_START_PLUGIN = "com.probeez.profiles.plugin.START_PLUGIN";
+	public static final String ACTION_STOP_PLUGIN = "com.probeez.profiles.plugin.STOP_PLUGIN";
+	public static final String ACTION_QUERY_PLUGIN_STATUS = "com.probeez.profiles.plugin.QUERY_PLUGIN_STATUS";
+	
+	public static final String EXTRA_SUMMARY = "summary";
+	public static final String EXTRA_STATE = "state";
+	public static final String EXTRA_STATES = "states";
+	public static final String EXTRA_PLUGIN_STATUS = "plugin_status";
+	public static final String EXTRA_PLUGIN_PACKAGE = "plugin_package";
+	
+	protected transient Context mContext;
+		
+
+	public PluginIntent(Context context) {
+		mContext = context;
+		setPackage("com.probeez.profiles");
+		ApplicationInfo appInfo = context.getApplicationInfo();
+		putExtra(EXTRA_PLUGIN_PACKAGE, appInfo.packageName);
+	}
+	
+	public PluginIntent(Context context, String action) {
+		this(context);
+		setAction(action);
+	}
+
+	public Bundle getState() {
+		Bundle state = getBundleExtra(EXTRA_STATE);
+		if (state==null) {
+			state = new Bundle();
+			putExtra(EXTRA_STATE, state);
+		}
+		return state;
+	}
+
+	public void send() {
+		mContext.sendBroadcast(this);
+	}
+	
+}
